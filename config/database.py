c6eff9eb2f5e1d17e2cdb2ef7a22b50411fdadaf
Htet Aung <htetaung20025q@example.com>
2026-08-11 12:56:41 +0630

    config files

--- config/database.py
+++ config/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from services.setting import settings
 
-engine = create_engine(settings.DATABASE_URI)
+engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
